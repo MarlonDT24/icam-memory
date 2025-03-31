8025fde3b38246c80416d400237b3661307eb26c
MarlonDT24 <torresmarlon40@gmail.com>
2025-03-31 16:20:49 +0200

    ultimo cambio hecho

--- app/Http/Controllers/LowVoltageController.php
+++ app/Http/Controllers/LowVoltageController.php
@@ -29,7 +29,7 @@ class LowVoltageController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,11 @@
 use App\Http\Controllers\FormController;
 use Illuminate\Support\Facades\Route;
 
+// Pagina principal para todas las memorias
 Route::get('/', [FormController::class, 'index'])->name('principal');
 
+
+// Se usa el FormController para las vistas de Compatibilidad
 Route::resource('form', FormController::class);
 
-Route::get('/form/{form}/convert-word', [FormController::class, 'convertToWord'])->name('form.convert.word');
-Route::get('/form/{form}/convert-pdf', [FormController::class, 'convertToPDF'])->name('form.convert.pdf');
-
+// Se usa el LowVoltageController para las vistas de Baja Tensión
