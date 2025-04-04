29cbe115f6f102248f97810a000371ae90ccb5ac
MarlonDT24 <torresmarlon40@gmail.com>
2025-04-04 09:11:04 +0200

    create de compatibility

--- resources/views/form/compatibility/create.blade.php
+++ resources/views/form/compatibility/create.blade.php
@@ -43,7 +43,6 @@
                             <label class="form-label" for="cover">Imagen (opcional):</label>
                             <input class="form-control" type="file" id="cover" name="cover" accept="image/*" data-bs-toggle="tooltip" title="Puede añadir una imagen">
                             @error('cover') <br>[ERROR]:{{ $message }} @enderror
-                            <br>
                         </div>
                     </div>
                     
@@ -63,21 +62,18 @@
                         <label class="form-label" for="cif">CIF del Titular:</label>
                         <input class="form-control" type="text" id="cif" name="cif" value="{{ old('cif') }}" data-bs-toggle="tooltip" title="Introduzca el CIF del titular">
                         @error('cif') <br>[ERROR]:{{ $message }} @enderror
-                        <br>
                     </div>
 
                     <div class="mb-3">
                         <label class="form-label" for="name_agent">Representante:</label>
                         <input class="form-control" type="text" id="name_agent" name="name_agent" value="{{ old('name_agent') }}" data-bs-toggle="tooltip" title="Introduzca el nombre del representante">
                         @error('name_agent') <br>[ERROR]:{{ $message }} @enderror
-                        <br>
                     </div>
 
                     <div class="mb-3">
                         <label class="form-label" for="nif">NIF del Representante:</label>
                         <input class="form-control" type="text" id="nif" name="nif" value="{{ old('nif') }}" data-bs-toggle="tooltip" title="Introduzca el NIF del representante">
                         @error('nif') <br>[ERROR]:{{ $message }} @enderror
-                        <br>
                     </div>
 
                     <h5 class="mt-4">Emplazamiento:</h5>
@@ -85,14 +81,12 @@
                         <label class="form-label" for="location">Dirección del Emplazamiento:</label>
                         <input class="form-control" type="text" id="location" name="location" value="{{ old('location') }}" data-bs-toggle="tooltip" title="Introduzca la dirección del emplazamiento">
                         @error('location') <br>[ERROR]:{{ $message }} @enderror
-                        <br>
                     </div>
 
                     <div class="mb-3">
                         <label class="form-label" for="cod_location">Código Postal del Emplazamiento:</label>
                         <input class="form-control" type="text" id="cod_location" name="cod_location" value="{{ old('cod_location') }}" data-bs-toggle="tooltip" title="Introduzca el código postal del emplazamiento">
                         @error('cod_location') <br>[ERROR]:{{ $message }} @enderror
-                        <br>
                     </div>
 
                     <h5 class="mt-4">Actividad:</h5>
@@ -100,14 +94,12 @@
                         <label class="form-label" for="activity">Actividad a realizar:</label>
                         <input class="form-control" type="text" id="activity" name="activity" value="{{ old('activity') }}" data-bs-toggle="tooltip" title="Introduzca la actividad que se va realizar">
                         @error('activity') <br>[ERROR]:{{ $message }} @enderror
-                        <br>
                     </div>
 
                     <div class="mb-3">
                         <label class="form-label" for="description">Descripción de la Actividad:</label>
                         <textarea class="form-control" id="description" name="description" rows="4" data-bs-toggle="tooltip" title="Introduzca una descripción que se va a realizar">{{ old('description') }}</textarea>
                         @error('description') <br>[ERROR]:{{ $message }} @enderror
-                        <br>
                     </div>
 
                     <h5 class="mt-4">Necesidades de uso y Aprovechamiento del Suelo:</h5>
@@ -115,14 +107,12 @@
                         <label class="form-label" for="m_parcels">Metros Cuadrados de la Parcela:</label>
                         <input class="form-control" type="text" id="m_parcels" name="m_parcels" value="{{ old('m_parcels') }}" data-bs-toggle="tooltip" title="Puede añadir decimales">
                         @error('m_parcels') <br>[ERROR]:{{ $message }} @enderror
-                        <br>
                     </div>
 
                     <div class="mb-3">
                         <label class="form-label" for="m_surface">Metros Cuadrados de la Superficie Edificada:</label>
                         <input class="form-control" type="text" id="m_surface" name="m_surface" value="{{ old('m_surface') }}" data-bs-toggle="tooltip" title="Puede añadir decimales">
                         @error('m_surface') <br>[ERROR]:{{ $message }} @enderror
-                        <br>
                     </div>
 
                     <h5 class="mt-4">Requerimientos de la Instalación respecto a los Servicios Públicos Municipales:</h5>
@@ -131,7 +121,6 @@
                         <textarea class="form-control" id="requirements" name="requirements" rows="6">{{ old('requirements',
                         "La instalación requiere de los servicios básicos de cualquier nave industrial. Estos servicios son los siguientes:\n\n• Conexión con red de agua municipal.\n• Conexión con red de alcantarillado.\n• Servicio de recogida de basuras.\n\nTambién se requiere conexión con la red eléctrica de Baja Tensión, aunque este servicio depende de la Compañía Suministradora.") }}</textarea>
                         @error('requirements') <br>[ERROR]:{{ $message }} @enderror
-                        <br>
                     </div>
 
                     <div class="d-flex justify-content-left">
