27b7269b469a7756913856d5f2f9246b283618cd
MarlonDT24 <torresmarlon40@gmail.com>
2025-04-04 13:49:28 +0200

    excel en el controlador

--- app/Http/Controllers/GroupElectroController.php
+++ app/Http/Controllers/GroupElectroController.php
@@ -6,6 +6,8 @@ use App\Models\GroupElectro;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\GroupElectroRequest;
 use Illuminate\Support\Facades\Storage;
+use PhpOffice\PhpWord\TemplateProcessor;
+use PhpOffice\PhpSpreadsheet\IOFactory;
 
 class GroupElectroController extends Controller
 {
@@ -32,6 +34,7 @@ class GroupElectroController extends Controller
     {
         $groupElectro = new GroupElectro();
         $groupElectro->name = $request->input('name');
+        $groupElectro->budget_excel = $request->input('budget_excel');
         $groupElectro->holder = $request->input('holder');
         $groupElectro->address = $request->input('address');
         $groupElectro->cod_address = $request->input('cod_address');
@@ -92,6 +95,11 @@ class GroupElectroController extends Controller
             // Metodo para guardar varios archivos en el mismo lugar (en este caso en el /storage/public)
             $groupElectro->cover = $request->file('cover')->store('covers', 'public');
         }
+        // En caso de que se suba el excel
+        if ($request->hasFile('budget_excel')) {
+            $groupElectro->budget_excel = $request->file('budget_excel')->store('budgets', 'public');
+        }
+
         //Despues de cazar todos los datos del formulario se guarda
         $groupElectro->save();
 
@@ -108,5 +116,53 @@ class GroupElectroController extends Controller
         }
         $groupElectro->delete();
         return redirect()->route('form.index');
+    }
+
+    public function getExcelDate($file)
+    {
+        $spreadsheet = IOFactory::load($file);
+        $sheet = $spreadsheet->getActiveSheet();
+
+        return [
+            'budget' => floatval($sheet->getCell('')->getValue()),
+            // Seguir colocando los demas datos del excel
+        ];
+
+    }
+
+    public function convertToWord(GroupElectro $groupElectro)
+    {
+        $templatePath = storage_path('app/private/plantillas/memoria_IEBT-modif-copia.doc');
+        $outputPath = storage_path('app/public/' . $groupElectro->name . '.doc');
+
+        $datesExcel = $this->getExcelDate(storage_path('app/private/excels'. $groupElectro->budget_excel));
+
+        $templateProcessor = new TemplateProcessor($templatePath);
+
+        $templateProcessor->setValue('name', $groupElectro->name);
+        $templateProcessor->setValue('holder', $groupElectro->holder);
+        $templateProcessor->setValue('address', $groupElectro->address);
+        $templateProcessor->setValue('cod_address', $groupElectro->cod_address);
+        $templateProcessor->setValue('cif', $groupElectro->cif);
+        $templateProcessor->setValue('name_agent', $groupElectro->name_agent);
+        $templateProcessor->setValue('nif', $groupElectro->nif);
+        $templateProcessor->setValue('location', $groupElectro->location);
+        $templateProcessor->setValue('cod_location', $groupElectro->cod_location);
+        $templateProcessor->setValue('name_location', $groupElectro->name_location);
+        $templateProcessor->setValue('build', $groupElectro->build);
+        $templateProcessor->setValue('kva', $groupElectro->kva);
+        $templateProcessor->setValue('kw', $groupElectro->kw);
+
+        $tensionText = $groupElectro->tension_type === '3F+N'
+            ? 'La tensión será alterna trifásica, de 400 V entre fases y 230 V entre fase y neutro.'
+            : 'La tensión será alterna, de 230 V entre fase y neutro.';
+        $templateProcessor->setValue('tension_type', $tensionText);
+
+        $presupuestoFormat = number_format($groupElectro->budget, 2, ',', '.');
+        $templateProcessor->setValue('presupuesto_total', $presupuestoFormat);
+
+        $templateProcessor->saveAs($outputPath);
+
+        return response()->download($outputPath)->deleteFileAfterSend(true);
     }
 }
